09cf943ebc6de563c3d8d88c9819344b6377b052
hykeem <w.hawk.95@gmail.com>
2022-02-25 17:08:56 +0900

    fix text

--- server/controller/signup.js
+++ server/controller/signup.js
@@ -3,46 +3,51 @@ const { user } = require('../models');
 const { hashPassword } = require('./functions/secure');
 
 module.exports = {
-    emailCheck: (req, res) => {
-      
-      const { userEmail } = req.query;
-        
-        user
-          .findOne({ where: { email: userEmail } })
-          .then((data) => {
-            if (data) {
-              return res.status(200).json({ success: false, message: '사용이 불가능한 이메일입니다' });
-            } else {
-              return res.status(404).json({ success: true, message: '사용이 가능한 이메일입니다' });
-            }
-          })
-          .catch((err) => console.log(err));
-    },
-    
-    nicknameCheck: (req, res) => {
-    
-      const { nickname } = req.query;
-      
-      user
-        .findOne({ where: { nickname } })
-        .then((data) => {
-          if (data) {
-            return res.status(200).json({ success: false, message: '사용이 불가능한 별명입니다' });
-          } else {
-            return res.status(404).json({ success: true, message: '사용이 가능한 별명입니다' });
-          }
-        })
-        .catch((err) => console.log(err));
-    },
+  emailCheck: (req, res) => {
+    const { userEmail } = req.query;
 
-    signup: async (req, res) => {
-        const { email, password, nickname } = req.body;
-        const hashPw = await hashPassword(password);
+    user
+      .findOne({ where: { email: userEmail } })
+      .then((data) => {
+        if (data) {
+          return res
+            .status(200)
+            .json({ success: false, message: '사용이 불가능한 이메일입니다' });
+        } else {
+          return res
+            .status(404)
+            .json({ success: true, message: '사용이 가능한 이메일입니다' });
+        }
+      })
+      .catch((err) => console.log(err));
+  },
 
-      user
-        .findOrCreate({
-          where: { userId },
-          defaults: { email, password: hashPw, nickname },
-        })
-    }
-}
+  nicknameCheck: (req, res) => {
+    const { nickname } = req.query;
+
+    user
+      .findOne({ where: { nickname } })
+      .then((data) => {
+        if (data) {
+          return res
+            .status(200)
+            .json({ success: false, message: '사용이 불가능한 별명입니다' });
+        } else {
+          return res
+            .status(404)
+            .json({ success: true, message: '사용이 가능한 별명입니다' });
+        }
+      })
+      .catch((err) => console.log(err));
+  },
+
+  signup: async (req, res) => {
+    const { email, password, nickname } = req.body;
+    const hashPw = await hashPassword(password);
+
+    user.findOrCreate({
+      where: { userId },
+      defaults: { email, password: hashPw, nickname },
+    });
+  },
+};
--- server/router/mypagePage.js
+++ server/router/mypagePage.js
@@ -4,5 +4,5 @@ const controller = require('../controller/mypage');
 
 router.get('/', controller.myInfo);
 router.patch('/users', controller.updateNickname);
-router.patch('users', controller.updatePassword);
+router.patch('/users', controller.updatePassword);
 router.delete('/users', controller.unregister);
