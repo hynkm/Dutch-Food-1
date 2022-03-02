9aa44df188fd189d02019bc1ce4966b84d31fb45
hhs3698 <83928355+hhs3698@users.noreply.github.com>
2022-03-03 01:24:39 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8080;
+const port = 8081;
 const { sequelize } = require('./models');
 
 const loginPage = require('./router/loginPage');
